2f0be597913cf1a16a22d18f0be36e9be77de921
ddavis1418 <ddavis1418@gmail.com>
2023-12-02 22:05:01 -0500

    Migrated to .Net 6 from .Net Core 3.1

---
+++ SQL_CRUD_CREATOR_TESTS/SqlCrudCreatorCoreTests.cs
@@ -0,0 +1,23 @@
+using SqlCrudCreatorCore.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SQL_CRUD_CREATOR_TESTS
+{
+    [TestClass]
+    public class SqlCrudCreatorCoreTests
+    {
+        IDatabaseService 
+         
+        [TestMethod]
+        public void CreateAllClassObjAndSQL()
+        {
+            //arrange
+
+            //act
+            
+            //assert
+            
+        }
+    }
+}
